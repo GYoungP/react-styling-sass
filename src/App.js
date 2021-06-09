7775540709ea0af8866705cd541d68d6a6164339
gyoung95 <nnnbdnnn@gmail.com>
2021-06-09 11:46:04 +0900

    scss 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from './components/Button';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons">
+        <Button size='large'>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size='smll'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color='gray' size='large'>BUTTON</Button>
+        <Button color='gray'>BUTTON</Button>
+        <Button color='gray' size='smll'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color='pink' size='large'>BUTTON</Button>
+        <Button color='pink'>BUTTON</Button>
+        <Button color='pink' size='smll'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' outline>BUTTON</Button>
+        <Button color='gray' outline>BUTTON</Button>
+        <Button color='pink' size='smll' outline>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' fullWidth>BUTTON</Button>
+        <Button color='gray' size='large' fullWidth>BUTTON</Button>
+        <Button color='pink' size='large' fullWidth
+        onClick = {() => {
+          console.log('클릭');
+        }}
+        onMouseMove={() => {
+          console.log('마우스무브');
+        }}
+        >BUTTON</Button>
+      </div>
     </div>
   );
 }
